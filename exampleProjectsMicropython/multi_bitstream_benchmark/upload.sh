5acf3f2b627b9644b4e621fd3497c2c19faf6e2c
Brooklyn Jennings <brooklynlj@outlook.com>
2025-07-23 12:08:50 -0400

    upload shell script for multi bitstream multi_bitstream_benchmark

---
+++ exampleProjectsMicropython/multi_bitstream_benchmark/upload.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mpremote fs cp bram_test.bin :
+mpremote fs cp dsp_test.bin :
+mpremote fs cp spram_test.bin :
+mpremote fs cp up5k_riscv1.bin :
+mpremote fs cp up5k_riscv2.bin :
+mpremote fs cp up5k_riscv3.bin :
+mpremote fs cp up5k_riscv4.bin :
+mpremote fs cp up5k_riscv5.bin :
+mpremote fs cp main2.py :main.py
