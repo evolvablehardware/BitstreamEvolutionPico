f2e56b1a48521eda36c659b8d434b33df23ae1d1
Brooklyn Jennings <brooklynlj@outlook.com>
2025-07-23 11:16:28 -0400

    update bitstream flashing with manual garbage collection

--- exampleProjectsMicropython/bitstream_to_CRAM/main.py
+++ exampleProjectsMicropython/bitstream_to_CRAM/main.py
@@ -1,6 +1,20 @@
 from machine import Pin
 import ice
-fpga = ice.fpga(cdone=Pin(ICE_DONE), clock=Pin(ICE_CLK), creset=Pin(ICE_RST), cram_cs=Pin(ICE_SS), cram_mosi=Pin(ICE_SI), cram_sck=Pin(ICE_SCK), frequency=48)
-file = open("bitstream.bin", "br")
+import gc
+
+#necessary to maintain speed after flashes
+def flash_with_collection(filename):
+    with open(filename,"rb") as f:
+        fpga.cram(f)
+        gc.collect()
+
+fpga = ice.fpga(cdone=Pin(ICE_DONE), 
+                clock=Pin(ICE_CLK), 
+                creset=Pin(ICE_RST), 
+                cram_cs=Pin(ICE_SS), 
+                cram_mosi=Pin(ICE_SI), 
+                cram_sck=Pin(ICE_SCK), 
+                frequency=48)
+
 fpga.start()
-fpga.cram(file)
+flash_with_collection("leds.bin")
